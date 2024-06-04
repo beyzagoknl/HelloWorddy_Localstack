f9202cad9968df3bac43724cbdd7d6aa7485bebf
beyzagoknl <beyzagoknl@gmail.com>
2024-06-04 23:53:59 +0200

    added files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,21 @@
+locals {
+  availability_zones = ["us-west-2a", "us-west-2b"]
+}
+
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.use_localstack ? "LKIAQAAAAAAAHFCVUJVW" : var.aws_access_key
+  secret_key = var.use_localstack ? "SiQEWV/O5tlMm9EugZ7PYVtLdsHDZnf/S59DSaud" : var.aws_secret_key
   region     = var.aws_region
+
+  skip_credentials_validation = var.use_localstack ? true : false
+  skip_metadata_api_check     = var.use_localstack ? true : false
+  skip_requesting_account_id  = var.use_localstack ? true : false
 
   endpoints {
     eks = var.use_localstack ? var.localstack_endpoint : null
+    ec2 = var.use_localstack ? var.localstack_endpoint : null
     ecr = var.use_localstack ? var.localstack_endpoint : null
+    iam = var.use_localstack ? var.localstack_endpoint : null
   }
 }
 
@@ -14,25 +24,29 @@ resource "aws_vpc" "eks_vpc" {
 }
 
 resource "aws_subnet" "eks_subnet" {
-  count = 2
+  count             = 2
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.${count.index}.0/24"
   availability_zone = element(local.availability_zones, count.index)
 }
 
+resource "aws_ecr_repository" "hello_world_repo" {
+  name = "hello-world-repo-unique"
+}
+
 resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
+  name = "eks-role-unique"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "eks.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
@@ -42,8 +56,8 @@ resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-cluster"
+resource "aws_eks_cluster" "new_cluster" {
+  name     = "new-cluster"
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
@@ -51,9 +65,31 @@ resource "aws_eks_cluster" "my_cluster" {
   }
 }
 
+resource "aws_iam_role" "node_group_role" {
+  name = "node-group-role-unique"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_policy_attachment" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
 resource "aws_eks_node_group" "worker_nodes" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "my-node-group"
+  cluster_name    = aws_eks_cluster.new_cluster.name
+  node_group_name = "new-node-group"
   node_role_arn   = aws_iam_role.node_group_role.arn
   subnet_ids      = aws_subnet.eks_subnet[*].id
 
@@ -62,26 +98,4 @@ resource "aws_eks_node_group" "worker_nodes" {
     max_size     = 2
     min_size     = 1
   }
-}
-
-resource "aws_iam_role" "node_group_role" {
-  name = "node-group-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_group_policy_attachment" {
-  role       = aws_iam_role.node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
